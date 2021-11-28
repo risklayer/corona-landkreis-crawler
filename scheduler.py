8c1b90d814515b18ef7b7f1f0e0a135cbf0e5ff5
Erich Schubert <erich.schubert@tu-dortmund.de>
2021-11-28 23:35:38 +0100

    scheduler --once

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,7 @@
 import botbase
 import random, time, sys
 from datetime import datetime
-from heapq import heapify, heapreplace
+from heapq import heapify, heapreplace, heappop
 
 import sh, hamburg, nds, bremen, nrw, hessen, rlp, bw, bayern, saarland, berlin, brandenburg, sachsen, sanhalt, thueringen
 
@@ -10,7 +10,7 @@ def scheduler(sheets):
     queue = [(task.next_time("init"), task) for task in botbase.schedule]
     if "--all" in sys.argv: queue = [(datetime.now(), task) for task in botbase.schedule]
     heapify(queue)
-    while True:
+    while len(queue) > 0:
         #botbase.todaystr = time.strftime("%d.%m.%Y") # update today value
         nextex, task = queue[0] # heap.peek()
         delay = (nextex - datetime.now()).total_seconds()
@@ -19,6 +19,9 @@ def scheduler(sheets):
             print("Waiting for %d seconds for AGS %05d %s at %s" % (delay, task.ags, task.fun.__name__, nextex))
             time.sleep(delay)
         success = task.run(sheets)
+        if "--once" in sys.argv:
+            heappop(queue)
+            continue # don't reschedule
         heapreplace(queue, (task.next_time(success), task))
 
 if __name__ == '__main__': scheduler(botbase.googlesheets())
